2692ddc378b6bae9665cfd7598e763028b4578d3
Uma Shanker Tiwari <shanker.tiwari3@gmail.com>
2015-06-09 17:32:20 +0530

    Initial commit for project.

--- USTLoggerSample/AppDelegate.h
+++ USTLoggerSample/AppDelegate.h
@@ -3,7 +3,7 @@
 //  USTLoggerSample
 //
 //  Created by Uma Shanker Tiwari on 09/06/15.
-//  Copyright (c) 2015 impressico. All rights reserved.
+//  Copyright (c) 2015 Uma Shanker Tiwari. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
